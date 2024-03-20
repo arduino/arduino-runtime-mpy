eec6f0f9adedb1650a512672d4de1d6bd71b51b7
Sebastian Romero <s.romero@arduino.cc>
2024-03-20 16:53:09 +0100

    Move meta data to init file

--- arduino/__init__.py
+++ arduino/__init__.py
@@ -1,1 +1,6 @@
+__author__ = "ubi de feo / murilo polese"
+__license__ = "MPL 2.0"
+__version__ = "0.1.0"
+__maintainer__ = "Arduino"
+
 from .arduino import *
--- arduino/arduino.py
+++ arduino/arduino.py
@@ -1,8 +1,3 @@
-__author__ = "ubi de feo / murilo polese"
-__license__ = "MPL 2.0"
-__version__ = "0.1.0"
-__maintainer__ = "Arduino"
-
 from machine import Pin, ADC, PWM
 from time import sleep_ms, ticks_us
 from random import randrange
