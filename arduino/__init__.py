c57617a069c633e4fdd6d67218cefbda7078fd2f
ubi de feo <me@ubidefeo.com>
2024-10-25 14:39:53 +0200

    Updated method names for map, credits (upon request of M. Polese) and version.
    Signed-off-by: ubi de feo <me@ubidefeo.com>

--- arduino/__init__.py
+++ arduino/__init__.py
@@ -1,7 +1,7 @@
-__author__ = "Murilo Polese"
+__author__ = "Ubi de Feo"
 __credits__ = ["Murilo Polese", "Ubi de Feo", "Sebastian Romero"]
 __license__ = "MPL 2.0"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __maintainer__ = "Arduino"
 
 from .arduino import *
