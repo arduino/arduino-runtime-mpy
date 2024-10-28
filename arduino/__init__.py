bb4382402e1de3e53cb723713f0aa43422a43d32
ubi de feo <me@ubidefeo.com>
2024-10-28 10:41:10 +0100

    Update version in __init__.py.
    Signed-off-by: ubi de feo <me@ubidefeo.com>

--- arduino/__init__.py
+++ arduino/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Murilo Polese"
 __credits__ = ["Murilo Polese", "Ubi de Feo", "Sebastian Romero"]
 __license__ = "MPL 2.0"
-__version__ = "0.1.0"
+__version__ = "0.4.0"
 __maintainer__ = "Arduino"
 
 from .arduino import *
